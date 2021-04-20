234927cc2d7f1cb053043380cf1d65ac54c46b31
unknown <akstr17coding@gmail.com>
2021-04-21 02:19:48 +0530

    move

--- Ping_Pong/main.cpp
+++ Ping_Pong/main.cpp
@@ -175,7 +175,21 @@ public:
 				else if (player1x == j && player1y == i)
 					cout << "\xDB"; //player1
 				else if (player2x == j && player2y == i)
-					cout << "\xDB"; //player1	
+					cout << "\xDB"; //player1
+
+				else if (player1x == j && player1y + 1 == i)
+					cout << "\xDB"; //player1
+				else if (player1x == j && player1y + 2 == i)
+					cout << "\xDB"; //player1
+				else if (player1x == j && player1y + 3 == i)
+					cout << "\xDB"; //player1
+
+				else if (player2x == j && player2y + 1 == i)
+					cout << "\xDB"; //player1
+				else if (player2x == j && player2y + 2 == i)
+					cout << "\xDB"; //player1
+				else if (player2x == j && player2y + 3 == i)
+					cout << "\xDB"; //player1
 				else
 					cout << " ";
 
@@ -191,7 +205,77 @@ public:
 			cout << "\xB2";
 		cout << endl;	
 	}
+	void Input()
+	{
+		ball->Move();
+
+		int ballx = ball->getX();
+		int bally = ball->getY();
+		int player1x = player1->getX();
+		int player2x = player2->getX();
+		int player1y = player1->getY();
+		int player2y = player2->getY();
+
+		if (_kbhit())
+		{
+			char current = _getch();
+			if (current == up1)
+				if (player1y > 0)
+					player1->moveUp();
+			if (current == up2)
+				if (player2y > 0)
+					player2->moveUp();
+			if (current == down1)
+				if (player1y + 4 < height)
+					player1->moveDown();
+			if (current == down2)
+				if (player2y + 4 < height)
+					player2->moveDown();
+
+			if (ball->getDirection() == STOP)
+				ball->randomDirection();
+
+			if (current == 'q')
+				quit = true;
+		}
+	}
+	void Logic()
+	{
+		int ballx = ball->getX();
+		int bally = ball->getY();
+		int player1x = player1->getX();
+		int player2x = player2->getX();
+		int player1y = player1->getY();
+		int player2y = player2->getY();
+
+		//left paddle
+		for (int i = 0; i < 4; i++)
+			if (ballx == player1x + 1)
+				if (bally == player1y + i)
+					ball->changeDirection((eDir)((rand() % 3) + 4));
+
+		//right paddle
+		for (int i = 0; i < 4; i++)
+			if (ballx == player2x - 1)
+				if (bally == player2y + i)
+					ball->changeDirection((eDir)((rand() % 3) + 1));
+
+		//bottom wall
+		if (bally == height - 1)
+			ball->changeDirection(ball->getDirection() == DOWNRIGHT ? UPRIGHT : UPLEFT);
+		//top wall
+		if (bally == 0)
+			ball->changeDirection(ball->getDirection() == UPRIGHT ? DOWNRIGHT : DOWNLEFT);
+		//right wall
+		if (ballx == width - 1)
+			ScoreUp(player1);
+		//left wall
+		if (ballx == 0)
+			ScoreUp(player2);
+	}
 };
+
+
 
 int main()
 {
